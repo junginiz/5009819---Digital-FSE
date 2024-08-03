e1a9c6017de48c0ada1c9f7c9f36c710ba482dd0
junginiz <97932841+junginiz@users.noreply.github.com>
2024-08-03 14:57:57 +0530

    Add files via upload

---
+++ Week 1/Design Patterns/Exercise 9/CommandPatternTest.java
@@ -0,0 +1,21 @@
+public class CommandPatternTest {
+    public static void main(String[] args) {
+        // Create receiver
+        Light livingRoomLight = new Light();
+
+        // Create command objects
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+        // Create invoker
+        RemoteControl remote = new RemoteControl();
+
+        // Turn the light on
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        // Turn the light off
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
---
+++ Week 1/Design Patterns/Exercise 9/LightOnCommand.java
@@ -0,0 +1,27 @@
+// LightOnCommand.java
+public class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+// LightOffCommand.java
+public class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
